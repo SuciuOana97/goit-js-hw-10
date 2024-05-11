cc161df7dde1bfb2902f2f3fa115c13f8f707bd5
Oanasuciu97 <luciana_bodeaoana@yahoo.com>
2024-05-11 23:26:47 +0300

    work

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,9 +1,8 @@
 import Notiflix from 'notiflix';
 import axios from 'axios';
 
-const api_key =
-  'live_gv9BA17aTwcuCrTNXGJ7bTGaQAmAqX0hq9nC7bkHZzcxRYWjt4OP9JOgnUsY3fJC';
-axios.defaults.headers.common['x-api-key'] = api_key;
+const api_key = '';
+axos.defaults.headers.common['x-api-key'] = api_key;
 const url = 'https://api.thecatapi.com/v1';
 
 export function fetchBreeds() {

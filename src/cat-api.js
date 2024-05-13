32709c6139393de6a65b71e91b473fba2c5b4808
Oanasuciu97 <luciana_bodeaoana@yahoo.com>
2024-05-13 09:40:48 +0300

    change something

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,8 +1,5 @@
-import Notiflix from 'notiflix';
 import axios from 'axios';
 
-const api_key =
-  'live_0w7ZrKBD6fKc50M4ptOmi3TdDtXGyczZRpLqygeOLtmb0vzvwDSj0pjMabDdD6yD';
 axios.defaults.headers.common['x-api-key'] =
   'live_0w7ZrKBD6fKc50M4ptOmi3TdDtXGyczZRpLqygeOLtmb0vzvwDSj0pjMabDdD6yD';
 const url = 'https://api.thecatapi.com/v1';
@@ -10,24 +7,22 @@ const fetchBreeds = () => {
   return axios
     .get('https://api.thecatapi.com/v1/breeds')
     .then(response => {
+      console.log(response.data);
       return response.data;
     })
     .catch(error => {
-      console.error('Error fetching data from API:', error);
-      throw error;
+      console.error('API Error');
     });
 };
 const fetchCatByBreed = breedId => {
   return axios
-    .get(
-      `https://api.thecatapi.com/v1/images/search?api_key=${apiKey}&breed_ids=${breedId}`
-    )
+    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
     .then(response => {
+      console.log(response.data);
       return response.data;
     })
     .catch(error => {
-      console.error('Error fetching data from API:', error);
-      throw error;
+      console.error('API Error');
     });
 };
 
